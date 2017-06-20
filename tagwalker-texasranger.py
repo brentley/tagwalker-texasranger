41044176a151fb8c5cbba2813afc5884ba5e6de9
root <brentley@oufan.com>
2017-06-20 16:49:15 -0400

    logging output, and exponential backoff with a maximum

--- tagwalker-texasranger.py
+++ tagwalker-texasranger.py
@@ -34,39 +34,48 @@ log.setLevel('INFO')
 
 def tag_check(instance):
     if instance.tags is None:
-        print("No tags defined")
+        log.info("there are no tags set for %s in region %s - we will terminate", instance.id, region)
         instance.terminate(instance.id)
     else:
         billing_tag = [tag['Value'] for tag in instance.tags if tag['Key'] == 'Billing']
         if not billing_tag:
-            log.info("there is no billing tag set for %s in region %s - we will terminate", instance.id, region)
             try:
-                print("No values in billing tag - terminating")
+                log.info("there is no billing tag set for %s in region %s - we will terminate", instance.id, region)
                 instance.terminate(instance.id)
             except Exception as e:
-                log.debug(e)
-                log.debug("Error when terminating %s", instance.id)
-                pass
+                if e.response['Error']['Code'] not in RETRY_EXCEPTIONS:
+                    log.error(e)
+                    log.error("Error when terminating %s", instance.id)
+                    pass
+                else:
+                    sleepvalue = (2 ** min(retries, 5))
+                    log.warning("We hit the rate limiter on instance %s, %s instances processed so far... sleeping for %s seconds, retries=%s", instance.id, instance_count, sleepvalue, retries)
+                    sleep(sleepvalue)
+                    retries += 1
 
 def set_termination_protection(instance):
-#    environment_dict = {}
     if instance.tags is None:
         print("No values in environment tag")
-#        return environment_dict
+        log.info("there are no tags set for %s in region %s", instance.id, region)
     environment_tag = [tag['Value'] for tag in instance.tags if tag['Key'] == 'Environment' and tag['Value'] == 'production']
     if environment_tag and not instance.spot_instance_request_id:
         apiterm = instance.describe_attribute(Attribute='disableApiTermination')
         apiterm = apiterm.get('DisableApiTermination')
         apiterm = apiterm.get('Value')
         if apiterm != True:
-            log.info("environment tag is set to production for %s in region %s - we will enable protection", instance.id, region)
             try:
-                #print("No values in enviromnent tag - nothing more to do")
+                log.info("environment tag is set to production for %s in region %s - we will enable protection", instance.id, region)
                 instance.modify_attribute(DisableApiTermination={'Value':True})
             except Exception as e:
-                log.debug(e)
-                log.debug("Error when setting termination protection  for %s", instance.id)
-                pass
+                if e.response['Error']['Code'] not in RETRY_EXCEPTIONS:
+                    log.error(e)
+                    log.error("Error when setting termination protection  for %s", instance.id)
+                    pass
+                else:
+                    sleepvalue = (2 ** min(retries, 5))
+                    log.warning("We hit the rate limiter on instance %s, %s instances processed so far... sleeping for %s seconds, retries=%s", instance.id, instance_count, sleepvalue, retries)
+                    sleep(sleepvalue)
+                    retries += 1
 
 def tag_cleanup(instance, detail):
     tempTags=[]
@@ -89,7 +98,7 @@ def tag_cleanup(instance, detail):
             tempTags.append(t)
     return(tempTags)
 
-def tagwalk(region):
+def terminator(region):
         log.info("Processing for region %s", region)
         ec2 = boto3.resource('ec2', region_name=str(region))
         instances = ec2.instances.filter(
@@ -106,10 +115,11 @@ def tagwalk(region):
                     log.error(e)
                     log.error("Error when processing instance %s in region %s", instance.id, region)
                     pass
-                print('We hit the rate limiter... backing off... retries={}'.format(retries))
-                #sleep(2 ** retries)
-                sleep(5)
-                retries += 1
+                else:
+                    sleepvalue = (2 ** min(retries, 5))
+                    log.warning("We hit the rate limiter on instance %s, %s instances processed so far... sleeping for %s seconds, retries=%s", instance.id, instance_count, sleepvalue, retries)
+                    sleep(sleepvalue)
+                    retries += 1
 
             try:
                 set_termination_protection(instance) # only on production non-spot instances
@@ -118,11 +128,23 @@ def tagwalk(region):
                     log.error(e)
                     log.error("Error when processing instance %s in region %s", instance.id, region)
                     pass
-                print('We hit the rate limiter... backing off... retries={}'.format(retries))
-                #sleep(2 ** retries)
-                sleep(5)
-                retries += 1
-
+                else:
+                    sleepvalue = (2 ** min(retries, 5))
+                    log.warning("We hit the rate limiter on instance %s, %s instances processed so far... sleeping for %s seconds, retries=%s", instance.id, instance_count, sleepvalue, retries)
+                    sleep(sleepvalue)
+                    retries += 1
+        log.info("Processed %s instances total", instance_count)
+                
+def tagwalk(region):
+        log.info("Processing for region %s", region)
+        ec2 = boto3.resource('ec2', region_name=str(region))
+        instances = ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'stopped', 'stopping']}])
+        retries = 0
+        instance_count = 0
+        for instance in instances:
+            log.debug("Processing instance %s in region %s", instance.id, region)
+            instance_count += 1
             try:
                 for vol in instance.volumes.all():
                     log.debug("Processing volume %s", vol.id)
@@ -142,10 +164,11 @@ def tagwalk(region):
                     log.error(e)
                     log.error("Error when processing instance %s in region %s", instance.id, region)
                     pass
-                print('We hit the rate limiter... backing off... retries={}'.format(retries))
-                #sleep(2 ** retries)
-                sleep(5)
-                retries += 1
+                else:
+                    sleepvalue = (2 ** min(retries, 5))
+                    log.warning("We hit the rate limiter on instance %s, %s instances processed so far... sleeping for %s seconds, retries=%s", instance.id, instance_count, sleepvalue, retries)
+                    sleep(sleepvalue)
+                    retries += 1
 
             try:
                 for interface in instance.network_interfaces:
@@ -162,15 +185,23 @@ def tagwalk(region):
                     log.error(e)
                     log.error("Error when processing instance %s in region %s", instance.id, region)
                     pass
-                print('We hit the rate limiter... backing off... retries={}'.format(retries))
-                #sleep(2 ** retries)
-                sleep(5)
-                retries += 1
+                else:
+                    sleepvalue = (2 ** min(retries, 5))
+                    log.warning("We hit the rate limiter on instance %s, %s instances processed so far... sleeping for %s seconds, retries=%s", instance.id, instance_count, sleepvalue, retries)
+                    sleep(sleepvalue)
+                    retries += 1
         log.info("Processed %s instances total", instance_count)
 
-log.info("Tagwalker Starting")
+log.info("Tagwalker Starting terminations")
+
+for region in regions:
+    terminator(region)
+    
+log.info("Tagwalker Starting copying tags")
+
 for region in regions:
     tagwalk(region)
+    
 log.info("Tagwalker Completed")
 
 # todo:
