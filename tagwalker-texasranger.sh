1f865144039a633365249bef5d0ac2ed229d0c46
root <brentley@oufan.com>
2017-06-20 11:49:55 -0400

    let the container crash if there is an error

--- tagwalker-texasranger.sh
+++ tagwalker-texasranger.sh
@@ -2,15 +2,8 @@
 
 set -ex
 
-delay=300 # time in sec to delay between checks - initially set to 5 minutes, but might need adjusting.
+delay=900 # time in sec to delay between checks - initially set to 5 minutes, but might need adjusting.
 
-run_tagwalker-texasranger() {
-  ./tagwalker-texasranger.py
-}
-
-while true; do
-  echo "$(date) - Running Tagwalker Texas Ranger NOW"
-  run_tagwalker-texasranger
-  echo "$(date) - Tagwalker Texas Ranger Done - sleeping for $delay seconds"
+while /usr/local/bin/tagwalker-texasranger.py; do
   sleep $delay
 done
