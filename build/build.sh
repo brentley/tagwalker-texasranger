0dc31664815eae924d2e4299b5a62cd1d89432dd
root <brentley@oufan.com>
2017-06-20 17:12:13 -0400

    point to internal registry

--- build/build.sh
+++ build/build.sh
@@ -2,11 +2,12 @@
 set -ux
 
 DIR="$PWD"
+DRAAS_REPO=ecs-services-registry-internal.ng.cloudpassage.com:5000
 GIT_SHA=$(git rev-parse HEAD 2>/dev/null | cut -c 1-7)
 version="${GIT_SHA}"
 name="$(basename $DIR)"
 name="tagwalker-texasranger"
-image="${image:-brentley/$name}"
+image="${image:-${DRAAS_REPO}/${name}}"
 
 docker build -t $image:$version .
 docker tag $image:$version $image:latest
--- push/push.sh
+++ push/push.sh
@@ -3,10 +3,12 @@ set -u
 
 DIR="$PWD"
 GIT_SHA=$(git rev-parse HEAD 2>/dev/null | cut -c 1-7)
+DRAAS_REPO=ecs-services-registry-internal.ng.cloudpassage.com:5000
 version="${GIT_SHA}"
 name="$(basename $DIR)"
 name="tagwalker-texasranger"
-image="${image:-brentley/$name}"
+image="${image:-${DRAAS_REPO}/${name}}"
+
 
 docker push $image:$version
 docker push $image:latest
